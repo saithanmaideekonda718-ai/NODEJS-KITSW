42b6188352d26c83b7ee7f560b998510e7cd08e3
saithanmaideekonda718-ai <saithanmaideekonda718@gmail.com>
2026-02-21 16:30:08 +0530

    updated

--- index9.js
+++ index9.js
@@ -1,4 +1,5 @@
 import express from "express"
+import bcrypt from "bcrypt"
 const app=express()
 app.listen(8080)
 app.use(express.json())
@@ -6,21 +7,27 @@ let users=[]
 app.get("/",(req,res)=>{
     res.send("hello world");
 });
-app.post("/signup",(req,res)=>{
-    const user=req.body
+app.post("/signup",async(req,res)=>{
+    const user=req.body;
+    const hashedPwd= await bcrypt.hash(req.body.password,10);
+    user.password=hashedPwd
     users.push(user);
     res.json(users);
 });
-app.post("/login",(req,res)=>{
-    const {email,password}=req.body;
-    const found=users.find(user=>user.email===email && user.password===password)
-    if(found){
-        res.send("Hello "+found.name)
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const found = users.find((user) => user.email === email);
+  if (found) {
+    const validate = await bcrypt.compare(password, found.password);
+    if (validate) {
+      res.send("Hello " + found.name);
+    } else {
+      res.send("Invalid Password");
     }
-    else{
-        res.send("User not found")
-    }
+  } else {
+    res.send("User not found");
+  }
 });
-app.get("/users",(req,res)=>{
-    res.json(users);
+app.get("/users", (req, res) => {
+  res.json(users);
 });
